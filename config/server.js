e9ca4adb14420f2587255b59add0b1f231ffba11
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2021-02-04 14:45:28 +0800

    Setup strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '751995bc798c4f57eb662454a0a34f76'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3e65e80-0e60-449d-ad7c-136ed3a42dc7'
+};
